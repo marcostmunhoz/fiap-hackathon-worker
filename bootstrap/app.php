f354d5b138a84f4ca0e25eec60da9cb68051bdd3
Marcos T Munhoz <3648104+marcostmunhoz@users.noreply.github.com>
2025-02-09 20:33:59 -0300

    fix: add missing web middleware group

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,11 +2,16 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Routing\Middleware\SubstituteBindings;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
     )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->group('web', [SubstituteBindings::class]);
+    })
     ->withCommands([
         App\Worker\Interface\Command\MessagingSubscribeCommand::class,
     ])
