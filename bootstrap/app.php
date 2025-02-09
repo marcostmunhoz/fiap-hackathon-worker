c24e6fde7d712ed5544e7eb1d0324e5c2681a265
Marcos T Munhoz <3648104+marcostmunhoz@users.noreply.github.com>
2025-02-09 20:21:16 -0300

    fix: move supervisord process to s6 layer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+    )
     ->withCommands([
         App\Worker\Interface\Command\MessagingSubscribeCommand::class,
     ])
--- terraform/cloud-run.tf
+++ terraform/cloud-run.tf
@@ -127,7 +127,7 @@ resource "google_cloud_run_service_iam_binding" "default" {
   service  = google_cloud_run_v2_service.worker_service.name
   role     = "roles/run.invoker"
   members = [
-    "serviceAccount:${data.google_service_account.service_account.email}"
+    "allUsers"
   ]
 }
 
@@ -137,9 +137,5 @@ resource "google_cloud_scheduler_job" "worker_heartbeat_scheduler" {
 
   http_target {
     uri = "${google_cloud_run_v2_service.worker_service.uri}/health"
-
-    oidc_token {
-      service_account_email = data.google_service_account.service_account.email
-    }
   }
 }
